75de026f50b55e86765a424fcf5508114bd67ef3
Strange-Philip <philipabakahmensah@gmail.com.com>
2023-12-25 16:13:26 +0000

    first commit

---
+++ ThemeApp/Helpers/ThemeChangeView.swift
@@ -0,0 +1,20 @@
+//
+//  ThemeChangeView.swift
+//  ThemeApp
+//
+//  Created by Philip Abakah on 25/12/2023.
+//
+
+import SwiftUI
+
+struct ThemeChangeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ThemeChangeView_Previews: PreviewProvider {
+    static var previews: some View {
+        ThemeChangeView()
+    }
+}
