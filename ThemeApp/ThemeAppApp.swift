2eae3fb2c77b78db47814170bd00b5b5bc0528bd
Strange-Philip <philipabakahmensah@gmail.com.com>
2023-12-25 13:49:58 +0000

    Initial Commit

---
+++ ThemeApp/ThemeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThemeAppApp.swift
+//  ThemeApp
+//
+//  Created by Philip Abakah on 25/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct ThemeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
